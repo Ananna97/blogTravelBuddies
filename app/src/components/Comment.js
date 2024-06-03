76289d2e28164f5ec868feb886d452eadf704b79
Ananna97 <ana_iancu97@yahoo.com>
2024-06-03 21:35:17 +0300

    add comment author

--- app/src/components/Comment.js
+++ app/src/components/Comment.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import { Card, CardContent, Typography } from '@mui/material';
+import React, {useEffect, useState} from 'react';
+import {Card, CardContent, CircularProgress, Typography} from '@mui/material';
+import {useParams} from "react-router-dom";
+import axios from "axios";
 
-const Comment = ({ comment }) => {
+const Comment = () => {
+    const { id } = useParams();
+    const [comment, setComment] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchComment = async () => {
+            try {
+                const response = await axios.get(`/comments/${id}`);
+                setComment(response.data);
+                setLoading(false);
+            } catch (error) {
+                setLoading(false);
+            }
+        };
+
+        fetchComment();
+    }, [id]);
+
+    if (loading) {
+        return <CircularProgress />;
+    }
+
     return (
         <Card variant="outlined" style={{ marginBottom: '10px' }}>
             <CardContent>
+                <Typography variant="body2" color="text.secondary" >
+                    <b>{comment.authorCommentFirstName} {comment.authorCommentLastName} : </b>
+                </Typography>
                 <Typography variant="body1">
                     {comment.text}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    Author: {comment.authorCommentFirstName} {comment.authorCommentLastName}
                 </Typography>
             </CardContent>
         </Card>
